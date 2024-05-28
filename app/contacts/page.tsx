5b1fd95a371288450dd349dcef4311b2e4c4d5f0
Zaidan07 <zaidanxgaming@gmail.com>
2024-05-28 22:44:02 +0700

    first

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -5,7 +5,9 @@ import Search from "@/components/search";
 const Cotacts = () => {
  return (
   <div className="max-w-screen-md mx-auto mt-5">
+    <h1 className="title text-2xl font-bold py-10">Data Barang</h1>
     <div className="flex items-center justify-between gap-1 mb-5">
+      
       <Search/>
       <CreateButton/>
     </div>
